7d63caddfaa9cb0fd76c39bb0cb7aec2b947ab7b
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-17 16:20:21 +1100

    name-animate-done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,14 +5,16 @@ import { useMediaQuery } from 'react-responsive';
 import { PerspectiveCamera } from '@react-three/drei';
 
 import Cube from '../components/Cube.jsx';
-import Rings from '../components/Rings.jsx';
 import ReactLogo from '../components/ReactLogo.jsx';
 import Button from '../components/Button.jsx';
-import Target from '../components/Target.jsx';
 import CanvasLoader from '../components/Loading.jsx';
 import HeroCamera from '../components/HeroCamera.jsx';
 import { calculateSizes } from '../constants/index.js';
 import { HackerRoom } from '../components/HackerRoom.jsx';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+
 
 const Hero = () => {
   // Use media queries to determine screen size
@@ -22,12 +24,28 @@ const Hero = () => {
 
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
+
+useGSAP(() => {
+  const words = document.querySelectorAll("#hero_text span");
+  gsap.from(words, {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    ease: "power3.out",
+    stagger: 0.2, // each word comes after 0.2s
+  });
+}, []);
+   
+
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Danucksan S <span className="waving-hand">👋</span>
-        </p>
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3" >
+        <p id="hero_text"
+  className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans"
+>
+  <span>Hi,</span> <span>I</span> <span>am</span> <span>Danucksan</span> <span>S</span> <span>👋</span>
+</p>
+
         <p className="hero_tag text-gray_gradient">Building <span className='text-green-900'>Web</span> & <span className='text-blue-900'>Mobile</span> Applications</p>
       </div>
 
